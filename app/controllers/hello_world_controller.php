f9dcd6f5887c840e742b4d93f65532fae8179a0a
ninakatila <nkatila@gmail.com>
2016-08-03 13:04:37 +0300

    vko 2 tehtävistä helloworld controlleri ja html sivujen suunnitelmista osa

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,15 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  echo 'Tämä on etusivu!';
     }
+    
+      /* public static function login(){
+     	View::make('login.html');
+    }*/
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -3,6 +3,10 @@
   $routes->get('/', function() {
     HelloWorldController::index();
   });
+  
+   /* $routes->get('/etusivu', function() {
+    HelloWorldController::login();
+  });*/
 
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
