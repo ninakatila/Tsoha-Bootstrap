0d70f2ceab748e65732a2bb28bbc80b08e110cbb
ninakatila <nkatila@gmail.com>
2016-08-16 09:51:53 +0300

    viikko 3 valmis, nyt toimii tietokantaan syöttökin uuden tehtävän osalta, korjattu task_controller metodi store.

--- app/controllers/task_controller.php
+++ app/controllers/task_controller.php
@@ -10,21 +10,23 @@ class TaskController extends BaseController{
         View::make('task/task.html', array ('task' => $task));        
     }
     
-    public static function store(){
+    public static function create(){
         View::make('task/new.html');
+    }
+
+    public static function store(){
         $params = $_POST;
         $task = new Task(array(
             'task_name' => $params ['task_name'],
             'task_status' => $params ['task_status'],
             'task_description' => $params ['task_description'],
             'deadline'=> $params ['deadline'],
-            'taski_importance' => $params ['task_importance']
+            'task_importance' => $params ['task_importance']
         ));
-        
-        Kint::dump($params);
-        $task ->save();
-        
-       // Redirect::to('/task/' .$task->id, array('message' => 'Tehtävä on lisätty muistilistaan'));
+        $task->save();
+                        
+       //Kysy miten tämän Redirectin pitäisi olla, sillä nyt ei toimi
+       //Redirect::to('/task/:id' .$task->id, array('message' => 'Tehtävä on lisätty muistilistaan'));
     }
 
 }
